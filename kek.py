5491c1ded6be24b32b939f5e3d21c6b3fc3d684d
gpiatek28 <76665206+gpiatek28@users.noreply.github.com>
2020-12-27 23:40:46 +0100

    Update kek.py

--- kek.py
+++ kek.py
@@ -1,4 +1,22 @@
-def hello():
-    print('Hay')
+import random
 
-hello()
+liczba = random.randint(1,20)
+
+print('Mam na myśli liczbę z zakresu od 1 do 20')
+
+for zgadywane in range (1,7):
+    print('Spróbuj odgadnąc liczbę')
+    guess = int(input())
+    print("próba:",zgadywane,"/6")
+
+    if guess > liczba:
+        print('Liczba jest zbyt duża')
+    elif guess < liczba:
+        print('Liczba jest za mala')
+    else:
+        break
+
+if guess == liczba:
+    print('Doskonale' +str(zgadywane))
+else:
+    print('nie udało sie, liczba to:'+str(liczba))
