0008964883625659ebb9ce326bf5e81c375aa906
gpiatek28 <76665206+gpiatek28@users.noreply.github.com>
2020-12-28 12:43:30 +0100

    Update kek.py

--- kek.py
+++ kek.py
@@ -1,22 +1,4 @@
-import random
+def romek(kek):
+    return kek/2
 
-liczba = random.randint(1,20)
-
-print('Mam na myśli liczbę z zakresu od 1 do 20')
-
-for zgadywane in range (1,7):
-    print('Spróbuj odgadnąc liczbę')
-    guess = int(input())
-    print("próba:",zgadywane,"/6")
-
-    if guess > liczba:
-        print('Liczba jest zbyt duża')
-    elif guess < liczba:
-        print('Liczba jest za mala')
-    else:
-        break
-
-if guess == liczba:
-    print('Doskonale' +str(zgadywane))
-else:
-    print('nie udało sie, liczba to:'+str(liczba))
+print(romek(5))
